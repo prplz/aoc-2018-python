4177f8598924c4c1ef5a1c008bd147ad33a003ec
Michael Himing <mhiming@gmail.com>
2019-01-05 16:22:08 +1100

    Simplify 08 part 2

--- 08/part2.py
+++ 08/part2.py
@@ -3,23 +3,20 @@ with open("in.txt") as f:
 
 result = 0
 child_count = [1]
-is_bottom = []
 meta_count = []
 values = [[]]
 while items:
     if child_count[-1] == 0:
         child_count.pop()
         child_count[-1] -= 1
-        if is_bottom.pop():
-            values.pop()
-            values[-1].append(sum(items.pop() for _ in range(meta_count.pop())))
+        v = values.pop()
+        meta = (items.pop() for _ in range(meta_count.pop()))
+        if v:
+            values[-1].append(sum(v[i - 1] for i in meta if 0 < i <= len(v)))
         else:
-            v = values.pop()
-            indices = [items.pop() for _ in range(meta_count.pop())]
-            values[-1].append(sum(v[i - 1] for i in indices if 0 < i <= len(v)))
+            values[-1].append(sum(meta))
     else:
         child_count.append(items.pop())
-        is_bottom.append(child_count[-1] == 0)
         meta_count.append(items.pop())
         values.append([])
 
