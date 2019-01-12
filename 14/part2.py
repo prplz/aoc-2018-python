fbf31fe79bb75c8409067cadfead406398543133
Michael Himing <mhiming@gmail.com>
2019-01-12 23:47:31 +1100

    I didn't use this

--- 14/part2.py
+++ 14/part2.py
@@ -1,6 +1,3 @@
-from itertools import count
-
-
 def solve(n):
     digits = [*map(int, str(n))]
     n_digits = len(digits)
@@ -8,14 +5,14 @@ def solve(n):
     index1 = 0
     index2 = 1
 
-    for i in count():
+    while True:
         new = recipes[index1] + recipes[index2]
         if new >= 10:
             recipes.append(1)
         recipes.append(new % 10)
         index1 = (index1 + 1 + recipes[index1]) % len(recipes)
         index2 = (index2 + 1 + recipes[index2]) % len(recipes)
-        if recipes[-n_digits :] == digits:
+        if recipes[-n_digits:] == digits:
             return len(recipes) - n_digits
         if recipes[-n_digits - 1 : -1] == digits:
             return len(recipes) - n_digits - 1
